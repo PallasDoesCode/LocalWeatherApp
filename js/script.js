c134086cff13a474c130d7f71e9e255c207a3647
RandomlyKnighted <thughes3@jsu.edu>
2016-03-03 22:44:05 -0600

    WIP - adding data to the page

--- js/script.js
+++ js/script.js
@@ -102,9 +102,76 @@
 
     weather.GetCurrentConditions().then( function( data ) {
 
-        var currentConditions = document.querySelector( "#today" );
-        currentConditions.innerHTML = data.weather[0].description;
+        // Set the current description
+        var currentConditionsDesc = document.querySelector( "#currentConditionsDesc" );
+        currentConditionsDesc.innerHTML = data.weather[0].description;
         console.log(data);
+
+        // Display wind information - Speed + Direction ( in degrees)
+        var windData = document.querySelector("#currentWindData");
+        var windDirection = "";
+
+        if (348.75 <= data.wind.deg && data.wind.deg <= 360) {
+            windDirection = "N";
+        }
+        else if (0 <= data.wind.deg && data.wind.deg <= 11.25) {
+            windDirection = "N";
+        }
+        else if (11.25 < data.wind.deg && data.wind.deg <= 33.75) {
+            windDirection = "NNE";
+        }
+        else if (33.75 < data.wind.deg && data.wind.deg <= 56.25) {
+            windDirection = "NE";
+        }
+        else if (56.25 < data.wind.deg && data.wind.deg <= 78.75) {
+            windDirection = "ENE";
+        }
+        else if (78.75 < data.wind.deg && data.wind.deg <= 101.25) {
+            windDirection = "E";
+        }
+        else if (101.25 < data.wind.deg && data.wind.deg <= 123.75) {
+            windDirection = "ESE";
+        }
+        else if (123.75 < data.wind.deg && data.wind.deg <= 146.25) {
+            windDirection = "SE";
+        }
+        else if (146.25 < data.wind.deg && data.wind.deg <= 168.75) {
+            windDirection = "SSE";
+        }
+        else if (168.75 < data.wind.deg && data.wind.deg <= 191.25) {
+            windDirection = "S";
+        }
+        else if (191.25 < data.wind.deg && data.wind.deg <= 213.75) {
+            windDirection = "SSW";
+        }
+        else if (213.75 < data.wind.deg && data.wind.deg <= 236.25) {
+            windDirection = "SW";
+        }
+        else if (236.25 < data.wind.deg && data.wind.deg <= 258.75) {
+            windDirection = "WSW";
+        }
+        else if (258.75 < data.wind.deg && data.wind.deg <= 281.25) {
+            windDirection = "W";
+        }
+        else if (281.25 < data.wind.deg && data.wind.deg <= 303.75) {
+            windDirection = "WNW";
+        }
+        else if (303.75 < data.wind.deg && data.wind.deg <= 326.25) {
+            windDirection = "NW";
+        }
+        else if (326.25 < data.wind.deg && data.wind.deg < 348.75) {
+            windDirection = "NNW";
+        }
+
+        windData.innerHTML = data.wind.speed + windDirection;
+
+        // Display humidity information (in percent)
+        var currentHumidity = document.querySelector("#currentHumidity");
+        currentHumidity.innerHTML = data.main.humidity;
+
+        // Display the pressure (in hPa)
+        var currentPressure = document.querySelector("#currentPressure");
+        currentPressure = data.main.pressure;
     })
 
     //weather.GetExtendedForecast();
